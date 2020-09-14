fc09487bf05fe0526d707e7e21da1b7d17a37e33
Kevin Boston <kbos22@gmail.com>
2020-09-14 15:29:27 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    CATEGORIES = ["Fiction", "Non-Fiction"]
+    validates :title, presence: true 
+    validates :category, inclusion: { in: CATEGORIES }
+    validates :content, length: { minimum: 100 }
 end
