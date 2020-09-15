49199682f79cb72ad56b9c2b913e7415910fbc0f
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-09-15 15:17:10 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
-class Post < ActiveRecord::Base
+class Post < ActiveRecord::Base 
+  validates_presence_of :title #  has an error for missing title
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) } #   has an error for invalid category
+  validates :content, length: { minimum: 100 } # has an error for too short content
 end
